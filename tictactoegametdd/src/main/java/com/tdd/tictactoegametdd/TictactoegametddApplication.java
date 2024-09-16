293c69187c474cf91f883b4a1e6615b5633b607e
reshm <reshm@DESKTOP-F10JV1T>
2024-09-16 16:10:43 +0530

    Initial Project Setup

---
+++ tictactoegametdd/src/main/java/com/tdd/tictactoegametdd/TictactoegametddApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.tictactoegametdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoegametddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoegametddApplication.class, args);
+	}
+
+}
